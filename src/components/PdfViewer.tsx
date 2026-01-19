72b244591ca5a07b577de7f2cca96410cc853e14
huiskesk <huiskesk@yahoo.com>
2026-01-19 14:38:32 -0800

    Use CDN PDF.js worker for PdfViewer

--- src/components/PdfViewer.tsx
+++ src/components/PdfViewer.tsx
@@ -7,10 +7,7 @@ import { useStore } from "../stores/useStore";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 interface PdfViewerProps {
   noteId: string;
